4aeb69b04247e445111c10dd4df916e7781768f4
RiccardoBagaglini <riccardo.bagaglini@tech.iagica.it>
2025-02-27 17:46:24 +0100

    Committo la prima variabile statica della classe per generare un conflitto guidato per il branch main

--- src/main/java/testGit1_RiccardoBagaglini.java
+++ src/main/java/testGit1_RiccardoBagaglini.java
@@ -1,4 +1,5 @@
 public class testGit1_RiccardoBagaglini{
    public static String variabileTestGitMain = "Variabile per il test di conflitto per il branch main";
    static String variabileTestGit = "Test conflitto Branch Sviluppo";
+   static String variabileTestGitMAin = "Test conflitto Branch Main";
 }
